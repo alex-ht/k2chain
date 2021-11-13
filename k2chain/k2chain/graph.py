4df047baeb6387661901fe5ea912b79efcf02ede
AlexHT Hung <hung_alex@icloud.com>
2021-11-13 11:40:43 +0800

    arcsort

--- k2chain/k2chain/graph.py
+++ k2chain/k2chain/graph.py
@@ -12,7 +12,7 @@ def chain_topo(symbols: k2.SymbolTable,
   Args:
     symbols:
       We assume that token IDs are contiguous (from 1 to `max_token`).
-      0 represents <blk>.
+      0 represents global <blk>.
     device:
       Optional. It can be either a string (e.g., 'cpu',
       'cuda:0') or a torch.device.
@@ -22,11 +22,13 @@ def chain_topo(symbols: k2.SymbolTable,
   '''
   max_int = symbols.ids()[-1]
   ext_symbols = _chain_expand_table(symbols)
-  fsa_str = [ "0 %d %d 0" % (s, s) for s in range(1, max_int+1)]
+  fsa_str = [ "0 0 0 0" ]
+  fsa_str += [ "0 %d %d 0" % (s, s) for s in range(1, max_int+1)]
   fsa_str += [ "%d %d %d 0" % (s, s*2, s*2) for s in range(1, max_int+1)]
   fsa_str += [ "%d %d %d 0" % (s*2, s*2, s*2) for s in range(1, max_int+1)]
   fsa_str += [ "%d 0 0 0" % (s) for s in range(1, max_int+1)]
-  fsa_str += [ "%d %d -1 0" % (s, max_int*2+1) for s in range(1, max_int+1)]
+  fsa_str += [ "%d %d -1 0" % (s, max_int*2+1) for s in range(0, max_int+1)]
+  fsa_str += [ "%d %d -1 0" % (s*2, max_int*2+1) for s in range(0, max_int+1)]
   fsa_str += [ str(max_int*2+1) ]
   fsa = k2.Fsa.from_str("\n".join(fsa_str))
-  return fsa, ext_symbols
+  return k2.arc_sort(fsa), ext_symbols
