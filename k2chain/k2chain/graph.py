0cc65a7d64bfe3eeb230809d47a5d29626f06128
AlexHT Hung <hung_alex@icloud.com>
2021-11-14 10:20:52 +0800

    update

--- k2chain/k2chain/graph.py
+++ k2chain/k2chain/graph.py
@@ -3,6 +3,7 @@ import torch
 from typing import Optional
 from typing import Union
 
+
 def _chain_expand_table(symbols: k2.SymbolTable) -> k2.SymbolTable:
     for s in symbols.symbols()[1:]:
         symbols.add("#" + s)
@@ -10,7 +11,7 @@ def _chain_expand_table(symbols: k2.SymbolTable) -> k2.SymbolTable:
 
 
 def chain_topo(symbols: k2.SymbolTable,
-            device: Optional[Union[torch.device, str]] = None) -> k2.Fsa:
+               device: Optional[Union[torch.device, str]] = None) -> k2.Fsa:
     '''Create a Chain topology.
     Args:
         symbols:
@@ -25,13 +26,13 @@ def chain_topo(symbols: k2.SymbolTable,
     '''
     max_int = symbols.ids()[-1]
     ext_symbols = _chain_expand_table(symbols)
-    fsa_str = [ "0 0 0 0" ]
-    fsa_str += [ "0 %d %d 0" % (s, s) for s in range(1, max_int+1) ]
-    fsa_str += [ "%d %d %d 0" % (s, s*2, s*2) for s in range(1, max_int+1) ]
-    fsa_str += [ "%d %d %d 0" % (s*2, s*2, s*2) for s in range(1, max_int+1) ]
-    fsa_str += [ "%d 0 0 0" % (s) for s in range(1, max_int+1) ]
-    fsa_str += [ "%d %d -1 0" % (s, max_int*2+1) for s in range(0, max_int+1) ]
-    fsa_str += [ "%d %d -1 0" % (s*2, max_int*2+1) for s in range(0, max_int+1) ]
-    fsa_str += [ str(max_int*2+1) ]
+    fsa_str = ["0 0 0 0"]
+    fsa_str += ["0 %d %d 0" % (s, s) for s in range(1, max_int+1)]
+    fsa_str += ["%d %d %d 0" % (s, s*2, s*2) for s in range(1, max_int+1)]
+    fsa_str += ["%d %d %d 0" % (s*2, s*2, s*2) for s in range(1, max_int+1)]
+    fsa_str += ["%d 0 0 0" % (s) for s in range(1, max_int+1)]
+    fsa_str += ["%d %d -1 0" % (s, max_int*2+1) for s in range(0, max_int+1)]
+    fsa_str += ["%d %d -1 0" % (s*2, max_int*2+1) for s in range(0, max_int+1)]
+    fsa_str += [str(max_int*2+1)]
     fsa = k2.Fsa.from_str("\n".join(fsa_str))
     return k2.arc_sort(fsa), ext_symbols
